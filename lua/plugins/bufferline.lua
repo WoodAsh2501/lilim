97c0a44577b45d00dfbc63cf1826ff582075ebf2
Redmi-Kaede <woodash2501@outlook.com>
2025-07-22 11:23:53 +0800

    plugin: bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+	"akinsho/bufferline.nvim",
+	opts = {},
+	offsets = {
+		{
+			filetype = "NvimTree",
+			text = "File Explorer",
+			text_align = "left",
+			separator = true,
+		},
+	},
+}
